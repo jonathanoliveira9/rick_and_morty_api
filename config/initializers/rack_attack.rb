52096b25b7073366668a91e85722c89b1097f0ad
jonathanoliveira9 <jonathanoliveirasilva9@gmail.com>
2021-01-07 22:49:53 -0300

    add rack-attack

---
+++ config/initializers/rack_attack.rb
@@ -0,0 +1,10 @@
+class Rack::Atack
+  Rack::Attack.cache.store = ActiveSupport::Cache::MemoryStore.new
+  safelist('allow-localhost') do |req|
+    '127.0.0.1' == req.ip || '::1' == req.ip
+  end
+
+  throttle('req/ip', limit: 10, period: 5) do |req|
+    req.ip
+  end
+end
